c054d958986ae1feb2ada07bd2a0a77a2d524a0e
Сухорученко Кирилл <k.sukhoruchenko@bars-open.ru>
2018-05-28 14:27:27 +0700

    добавлены try..catch, домашняя директория вынесена в переменную, подправлен вывод сообщений в файл

--- exmo.py
+++ exmo.py
@@ -4,16 +4,19 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from pynput.keyboard import Key, Listener
-import pyautogui
 import datetime
+import time
 import ast
+import sys
 
-browser = webdriver.Chrome('/home/kirill/chromedriver')
+homepath = '/home/shinod/'
+
+browser = webdriver.Chrome(homepath+'chromedriver')
 browser.get('https://exmo.me/ru/login')
 
 timeout = 10
-myhbz = 10000
-myhbz_price = 0.0162
+myhbz = 9959
+myhbz_price = 0.0152
 can_sell = 1
 myusd = 0
 try:
@@ -40,9 +43,9 @@ def on_press(key):
             price = price_td.text
             print(price)
             content.click()
-            with open('/home/kirill/exmo/result.txt', 'a') as file:
+            with open(homepath+'exmo/result.txt', 'a') as file:
                 file.write(
-                    'Продано: ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price)+ ' Количество ' + str(myhbz) + ' На счету ' + str(myusd))
+                    'Продано: ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price)+ ' Количество ' + str(myhbz) + ' На счету ' + str(myusd) + '\n')
         if key_press.char == 's':
             content = browser.find_element_by_css_selector('#mCSB_6_container .trade_table tbody tr:nth-of-type(2)')
             price_td = content.find_element_by_class_name('tcol_price')
@@ -54,49 +57,64 @@ def on_press(key):
 
 
 def auto_broker():
-        global myhbz, myhbz_price, myusd, can_sell
+        global myhbz, myhbz_price, myusd, can_sell, homepath
         while 0 != 1:
+            time.sleep(1)
             if myhbz != 0 and int(can_sell) == 1:
-                content = browser.find_element_by_css_selector('#mCSB_6_container .trade_table tbody tr:nth-of-type(2)')
-                price_td = content.find_element_by_class_name('tcol_price')
-                count_td = content.find_element_by_class_name('tcol_count')
-                price = price_td.text
-                count = count_td.text
-                if ast.literal_eval(price) > myhbz_price*1.01:
-                    if ast.literal_eval(count) >ast.literal_eval(myhbz):
-                        myusd = ast.literal_eval(myhbz) * ast.literal_eval(price)
-                        with open('/home/kirill/exmo/result.txt', 'a') as file:
-                            file.write('Продано: '+datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price) + ' Количество ' + str(myhbz) + ' На счету ' + str(myusd))
-                        myhbz = 0
-                    else:
-                        myusd = ast.literal_eval(count) * ast.literal_eval(price)
-                        with open('/home/kirill/exmo/result.txt', 'a') as file:
-                            file.write('Продано: ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price) + ' Количество ' + str(count) + ' На счету ' + str(myusd))
-                        myhbz = myhbz - ast.literal_eval(count)
-                    if myhbz == 0:
-                        myhbz_price = price
-                        can_sell = 0
+                try:
+                    content = browser.find_element_by_css_selector('#mCSB_5_container .trade_table tbody tr:nth-of-type(2)')
+                    price_td = content.find_element_by_class_name('tcol_price')
+                    count_td = content.find_element_by_class_name('tcol_count')
+                    price = price_td.text
+                    count = count_td.text
+                    print(str(float(price)))
+                    if float(price) > float(myhbz_price)*1.01:
+                        print(str(float(price)) + ' ' + str(float(myhbz_price)))
+                        if float(count) >float(myhbz):
+                            myusd = myusd + float(myhbz) * float(price)
+                            myhbz = 0
+                            with open(homepath + 'exmo/result.txt', 'a') as file:
+                                file.write('Продано: ' + datetime.datetime.now().strftime(
+                                    "%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price) + ' Количество ' + str(
+                                    myhbz) + ' На счету ' + str(myusd) + ' HBZ ' + str(myhbz) + '\n')
+                        else:
+                            myusd = myusd + float(count) * float(price)
+                            myhbz = float(myhbz) - float(count)
+                            with open(homepath + 'exmo/result.txt', 'a') as file:
+                                file.write('Продано: ' + datetime.datetime.now().strftime(
+                                    "%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price) + ' Количество ' + str(
+                                    count) + ' На счету ' + str(myusd) + ' HBZ ' + str(myhbz) + '\n')
+                        if float(myhbz) == 0:
+                            myhbz_price = price
+                            can_sell = 0
+                except:
+                    print("Снова не найден: ", sys.exc_info()[1])
             else:
-                content = browser.find_element_by_css_selector('#mCSB_5_container .trade_table tbody tr:nth-of-type(2)')
-                price_td = content.find_element_by_class_name('tcol_price')
-                count_td = content.find_element_by_class_name('tcol_count')
-                price = price_td.text
-                count = count_td.text
-                if ast.literal_eval(price) < myhbz_price*1.01:
-                    if ast.literal_eval(count) * ast.literal_eval(price) > myusd:
-                        count = ast.literal_eval(myusd) / ast.literal_eval(price)
-                        myhbz = myhbz + count
-                        myusd = 0
-                        with open('/home/kirill/exmo/result.txt', 'a') as file:
-                            file.write('Куплено: ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price) + ' Количество ' + str(count) + ' На счету ' + str(myusd))
-                    else:
-                        myhbz = myhbz + count
-                        myusd = myusd - ast.literal_eval(price) * ast.literal_eval(count)
-                        with open('/home/kirill/exmo/result.txt', 'a') as file:
-                            file.write(
-                                'Куплено: ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price) + ' Количество ' + str(count) + ' На счету ' + str(myusd))
-                    if myusd == 0:
-                        can_sell = 1
+                try:
+                    content = browser.find_element_by_css_selector('#mCSB_4_container .trade_table tbody tr:nth-of-type(2)')
+                    price_td = content.find_element_by_class_name('tcol_price')
+                    count_td = content.find_element_by_class_name('tcol_count')
+                    price = price_td.text
+                    count = count_td.text
+                    print(price)
+                    if float(price) < float(myhbz_price)*0.99:
+                        print(str(float(price)) + ' ' + str(float(myhbz_price)))
+                        if float(count) * float(price) > float(myusd):
+                            count = float(myusd) / float(price)
+                            myhbz = float(myhbz) + float(count)
+                            myusd = 0
+                            with open(homepath+'exmo/result.txt', 'a') as file:
+                                file.write('Куплено: ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price) + ' Количество ' + str(count) + ' На счету ' + str(myusd) + ' HBZ ' + str(myhbz) + '\n')
+                        else:
+                            myhbz = float(myhbz) + float(count)
+                            myusd = float(myusd) - float(price) * float(count)
+                            with open(homepath+'exmo/result.txt', 'a') as file:
+                                file.write(
+                                    'Куплено: ' + datetime.datetime.now().strftime("%d-%m-%Y %H:%M:%S") + ' Цена ' + str(price) + ' Количество ' + str(count) + ' На счету ' + str(myusd) + ' HBZ ' + str(myhbz) + '\n')
+                        if float(myusd) == 0:
+                            can_sell = 1
+                except:
+                    print("Снова не найден при продаже: ", sys.exc_info()[1])
 
 
 with Listener(on_press=on_press) as listener:
